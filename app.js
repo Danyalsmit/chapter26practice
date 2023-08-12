ade490b6164db90f3406435492651c92bb51cb4c
Danyalsmit <121617130+Danyalsmit@users.noreply.github.com>
2023-08-12 13:11:46 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// var data="hello,  my name is ghous ahmed!";    //find chARACTER
+// for(var i=0; i< data.length ; i++){
+// console.log(data.charAt(i));
+// }
+// var data="hello,  my name is ghous ahmed! ghous";   //replace 1 character
+// console.log(data.replace("ghous","danyal"))
+
+//  var data="hello,  my name is ghous ahmed! ghous";   //replace all character
+// console.log(data.replaceAll("ghous","danyal"))
+
+// Math.round(2.9)
+// 3
+
+// var num =2.4;
+// console.log(Math.ceil(num));  3
+// console.log(Math.round(num)); 2
+// console.log(Math.floor(num)); 2
+
+// var one=prompt("enter player one name")
+// var two=prompt("enter player one name")
+// var result= Math.random()*1;
+// var kol = Math.round(result);
+// if(kol===1){
+//     alert("head you win")
+//   }  
+// else(kol===0)
+// alert("tail you win ")
+// console.log(kol)
+// var dk = parseInt("25.76")    //string convert to number
+// var kd = parseFloat("25.76")  // string convert to decimal values
+//  var lk= Number("2576")        // number replace both parseint and parse float     
+// console.log(dk,kd,lk)
+// var one=12345;
+// var home="123456";
+// console.log(Number(home))      
+// console.log(one.toString())         .tostring convert number to string//
+ 
+// var num=123.567;
+// console.log(num.toFixed(1));            // decimal k bd jitne value lane hn tofixed use kro
